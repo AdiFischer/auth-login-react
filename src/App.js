e29fd3400c0e6634d9f6d5443b2fe320f6909651
AdiFischer <adifischer1@gmail.com>
2022-11-05 11:08:11 -0400

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { useState } from 'react';
 import {Routes, Route} from 'react-router-dom'
 import Profile from './components/Profile';
-import Login from './components/Login';
 import Signup from './components/Signup';
+import Login from './components/Login';
 import './App.css';
 
 export default function App() {
   const [user, setUser] = useState()
+  const [token, setToken] = useState()
   return (
    <Routes>
     {user
-    ? <Profile user={user}/>
+    ? <Route path='*' element={<Profile token={token} setUser={setUser} user={user}/>}/>
     : <>
-    <Route path='/signup' element={<Signup setUser={setUser}/>}/>
-    <Route path='*' element={<Login setUser={setUser}/>}/>
+    <Route path='/signup' element={<Signup setToken={setToken} setUser={setUser}/>}/>
+    <Route path='*' element={<Login setToken={setToken} setUser={setUser}/>}/>
     </>
     }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,18 @@
 import { Button, Form, Input, Layout } from 'antd'
 
-export default function Login() {
+export default function Login({setUser, setToken}) {
     const handleFormSubmit = (values) => {
-        console.log('Success:', values)
+        fetch('http://localhost:3030/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(values)
+        })
+        .then(response => response.json())
+        .then(data=> {
+            setToken(data.token)
+            setUser(data.user)
+        })
+        .catch(alert)
     }
     return (
         <Layout.Content style={{ padding: '50px' }}>
@@ -23,9 +33,7 @@ export default function Login() {
                 <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
                     <Button type='primary' htmlType='submit'>Login</Button>
                 </Form.Item>
-
             </Form>
-
         </Layout.Content>
     )
 }
